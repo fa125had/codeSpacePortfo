6c088902a58386a1dfccbee5a3ac6855ee6097ab
Farshad <101581788+fa125had@users.noreply.github.com>
2023-12-18 10:37:04 +0000

    changed home bg, changed contact

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Farshad Abbaspour",
+  title: "Full Stack JavaScript Developer",
+  email: "Far5had@outlook.com",
+  gitHub: "fa125had",
+  instagram: "_far5had",
+  linkedIn: "far5had",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
